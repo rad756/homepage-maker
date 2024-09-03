6d747fad413c35b8e7cabb7cd068dfdbefb10046
rad756 <radek9616@gmail.com>
2024-09-03 20:04:00 +0100

    Create button and displaing it

--- logic/website.go
+++ logic/website.go
@@ -1,7 +1,10 @@
 package logic
 
+import "fyne.io/fyne/v2"
+
 type Website struct {
-	Name string // Name for website button label
-	URL  string // Link to the website
-	Icon string // Path to icon/favicon of website MAYBE MAKE IT FYNE URI TYPE
+	Name     string   // Name for website button label
+	URL      string   // Link to the website
+	Icon     fyne.URI // Path to icon/favicon
+	Selected bool     // Show or not buttons in border to move/re-order button in grid
 }
--- ui/gui.go
+++ ui/gui.go
@@ -3,7 +3,9 @@ package ui
 import (
 	"homepage-maker/logic"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -14,11 +16,44 @@ func LoadGUI(MyApp *logic.MyApp) {
 func LoadMainMenu(MyApp *logic.MyApp) {
 	settingsBtn := widget.NewButton("Settings", nil)
 
-	content := container.NewBorder(nil, settingsBtn, nil, nil, nil)
+	mainContent := LoadMainContent(*MyApp)
 
-	MyApp.Win.SetContent(content)
+	allContent := container.NewBorder(nil, settingsBtn, nil, nil, mainContent)
+
+	MyApp.Win.SetContent(allContent)
 }
 
 func LoadSetupMenu(MyApp *logic.MyApp) {
 
+}
+
+func MakeWebsiteButton(Website logic.Website, MyApp *logic.MyApp) *fyne.Container {
+	upBtn := widget.NewButtonWithIcon("", theme.MoveUpIcon(), nil)
+	downBtn := widget.NewButtonWithIcon("", theme.MoveDownIcon(), nil)
+	leftBtn := widget.NewButtonWithIcon("", theme.NavigateBackIcon(), nil)
+	rightBtn := widget.NewButtonWithIcon("", theme.NavigateNextIcon(), nil)
+	mainBtn := widget.NewButtonWithIcon("", theme.ContentAddIcon(), nil)
+	lbl := widget.NewLabel(Website.Name)
+
+	insideBorder := container.NewBorder(nil, lbl, nil, nil, mainBtn)
+
+	if Website.Selected {
+		return container.NewBorder(upBtn, downBtn, leftBtn, rightBtn, insideBorder)
+	} else {
+		return container.NewBorder(nil, nil, nil, nil, insideBorder)
+	}
+}
+
+func MakeBlankButton(MyApp *logic.MyApp) *fyne.Container {
+	mainBtn := widget.NewButtonWithIcon("", theme.ContentAddIcon(), nil)
+
+	return container.NewBorder(nil, nil, nil, nil, mainBtn)
+}
+
+func LoadMainContent(MyApp logic.MyApp) *fyne.Container {
+	if len(MyApp.Content) == 0 {
+		return MakeBlankButton(&MyApp)
+	} else {
+		return container.NewVBox(nil)
+	}
 }
